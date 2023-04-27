9b2afe52bc71a66e2ab8f314ad5dab96d6a2b7b8
Sergei Tarasov <sergxf@yandex.ru>
2023-04-27 18:16:42 +0300

    new vals

--- R_phylo-RD/R/sim_BM-functions.R
+++ R_phylo-RD/R/sim_BM-functions.R
@@ -9,20 +9,20 @@
 # Arguments
 # Du(Da), Dv(Db), f, k
 # ---------------
-pars <- list(
+sim_pars <- list(
   nSim = 1,
   nTaxa = 10,
   treeScale = 100,
   birthRate = 1,
   deathRate = 0,
   #               Du(Da), Dv(Db), f, k
-  parsFixed   = c(0.16, NA, 0.035, NA),
-  rootValues  = c(0, 0.08, 0, 0.065),
+  parsFixed   = c(0.16, NA, 0.035, 0.065),
+  rootValues  = c(0, 0.06, 0, 0.065),
   mu          = c(0, 0, 0, 0),
-  sig2        = c(0, 0.1, 0, 0.1),
+  sig2        = c(0, 0.001, 0, 0),
 
   bmBounds = matrix(c(0, 0,
-                      0, 1,
+                      0.05, 0.08,
                       0, 0,
                       0, 1), 4,2, byrow = TRUE)
 )
@@ -30,25 +30,29 @@ pars <- list(
 
 # ---------------
 
-
-simTreeBM <- function(pars){
-  tree<-pbtree(n=nTaxa, scale=treeScale, b=birthRate, d=deathRate, nsim = 1)
-
+simTreeBM(sim_pars)
+simTreeBM <- function(sim_pars){
+  tree<-pbtree(n=sim_pars$nTaxa, scale=sim_pars$treeScale, b=sim_pars$birthRate, d=sim_pars$deathRate, nsim = 1)
   traits <- c()
   # Simulate Traits
   # i=1
   for (i in c(1:4)){
-    if (is.na(parsFixed[i])){
-      tip_values <- fastBM(tree, a=rootValues[i], mu=mu[i], sig2=sig2[i], bounds=bmBounds[i,], internal=FALSE, nsim=1)
+    if (is.na(sim_pars$parsFixed[i])){
+      tip_values <- fastBM(tree, a=sim_pars$rootValues[i], mu=sim_pars$mu[i], sig2=sim_pars$sig2[i],
+                           bounds=sim_pars$bmBounds[i,], internal=FALSE, nsim=1)
       # print(tip_values)
       traits <- cbind(traits, tip_values)
-    } else if (is.numeric(parsFixed[i])) {
-      tip_values <-rep(parsFixed[i], nTaxa)
+    } else if (is.numeric(sim_pars$parsFixed[i])) {
+      tip_values <-rep(sim_pars$parsFixed[i], sim_pars$nTaxa)
       traits <- cbind(traits, tip_values)
     } else {
       print('Error: unknown format')
     }
   }
   colnames(traits) <- c('Du', 'Dv', 'f', 'k')
-  return(t)
+  lst <- split(traits, row(traits))
+  # tree
+  tree_str <- write.tree(tree, append = FALSE, digits = 10, tree.names = FALSE)
+  data <- list(tree=tree_str, tip_traits = lst)
+  return(data)
 }
